6976ff38fae122f3e67c444fe5f47c5395eae4f7
wisniewskikr <wisniewskikr@gmail.com>
2023-12-10 17:31:23 +0100

    Implement MyHttpServer

--- java-helloworld-httphandler/src/main/java/com/example/Main.java
+++ java-helloworld-httphandler/src/main/java/com/example/Main.java
@@ -1,11 +1,11 @@
 package com.example;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {		
-				 
-		System.out.println("Hello World!");
-
+	public static void main(String[] args) throws IOException {				 
+		MyHttpServer.displayHelloWorld();
 	}
 
 	
