55c48ec8c203effcc052bfd1af1bbfaa2e6166d6
wisniewskikr <wisniewskikr@gmail.com>
2023-12-11 07:42:03 +0100

    Implement displayMessage

--- java-helloworld-serversocket/src/main/java/com/example/MyServerSocket.java
+++ java-helloworld-serversocket/src/main/java/com/example/MyServerSocket.java
@@ -34,8 +34,30 @@ public class MyServerSocket {
         BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
         BufferedWriter out = new BufferedWriter((new OutputStreamWriter(client.getOutputStream())));
 
+        String line = in.readLine();
+        String[] header = line.split(" ");
+        if (header[0].equals("GET")) {
+            handleGetMethod(header[1], out);
+        }
+
         in.close();
         out.close();
+
+    }
+
+    private static void handleGetMethod(String path, BufferedWriter out) throws IOException {
+
+        String response = "Hello World!";
+        
+        if (!"/".equals(path)) {
+            return;
+        }        
+
+        out.write("HTTP/1.1 " + 200 + "\r\n");
+        out.write("Content-Length: " + response.length() + "\r\n");
+        out.write("\r\n");
+        out.write(response);
+        out.flush();
 
     }
     
