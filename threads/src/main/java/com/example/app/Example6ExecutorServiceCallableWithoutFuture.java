c39f3bf04cbfdf5c18ac71520de4678e19670d51
wisniewskikr <wisniewskikr@gmail.com>
2024-01-08 08:53:46 +0100

    Init Example6ExecutorServiceCallableWithoutFuture

---
+++ threads/src/main/java/com/example/app/Example6ExecutorServiceCallableWithoutFuture.java
@@ -0,0 +1,36 @@
+package com.example.app;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.example.app.utils.CountUtil;
+
+public class Example6ExecutorServiceCallableWithoutFuture {
+
+	public static void main(String[] args) {
+	
+		Example6ExecutorServiceCallableWithoutFuture main = new Example6ExecutorServiceCallableWithoutFuture();			
+		main.countByExecutorService(2);
+		
+	}
+	
+	public void countByExecutorService(int threadsCount) {
+			
+		ExecutorService executorService = Executors.newFixedThreadPool(threadsCount);
+		for (int i = 1; i <= threadsCount; i++) {			
+			executorService.submit(new Example6CustomCallable());			
+		}
+		executorService.shutdown();		
+			
+	}
+
+}
+
+class Example6CustomCallable implements Runnable {
+
+	@Override
+	public void run() {
+		CountUtil.count();				
+	}
+	
+}
