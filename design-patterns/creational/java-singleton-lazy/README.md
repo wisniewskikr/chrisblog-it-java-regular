6c5e30384e16a8709536ee1aaf260d10189283a1
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-19 10:32:46 +0200

    Init singleton lazy

---
+++ design-patterns/creational/java-singleton-lazy/README.md
@@ -0,0 +1,7 @@
+## Goal
+
+Goal of this project is to create **Singleton** creational design pattern type **eager**.  
+
+## Explanetion
+
+**Lazy Initialization**: The instance is created only when it is needed, as opposed to eager initialization where the instance is created at the time of class loading.
---
+++ design-patterns/creational/java-singleton-lazy/src/Singleton.java
@@ -0,0 +1,22 @@
+
+
+public class Singleton {
+
+    private static Singleton instance = null;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+
+    }
+
+    public void showMessage() {
+        System.out.println("Hello World!");
+    }
+
+}
