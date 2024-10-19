354e95419ded577dc63e84fa1ae681937382f76f
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-19 10:39:54 +0200

    Implement double-checked locking

---
+++ design-patterns/creational/java-singleton-double-checked-locking/README.md
@@ -0,0 +1,7 @@
+## Goal
+
+Goal of this project is to create **Singleton** creational design pattern type **double-checked locking**.  
+
+## Explanetion
+
+**The double-checked locking** technique ensures that the singleton instance is created only once and is thread-safe without the overhead of synchronization every time the method is called.
---
+++ design-patterns/creational/java-singleton-double-checked-locking/src/Singleton.java
@@ -0,0 +1,27 @@
+
+
+public class Singleton {
+
+    private static Singleton instance = null;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+
+        if (instance == null) {
+            synchronized(Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+            
+        }
+        return instance;
+
+    }
+
+    public void showMessage() {
+        System.out.println("Hello World!");
+    }
+
+}
