8b914fdb3923b53c5ebf1e46d535d67e894d9bc8
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-22 12:49:55 +0200

    Update quick sort

--- interview/sorting-algorithms/java-quick-sort-asc-array-elements/README.md
+++ interview/sorting-algorithms/java-quick-sort-asc-array-elements/README.md
@@ -1,9 +1,19 @@
 ## Goal
 
-Goal of this project is to present how to sort array in Java using **quick sort** strategy.
+Goal of this project is to present how to sort array in Java using **Quick Sort** strategy. Quick Sort is a highly efficient sorting algorithm that uses a divide-and-conquer strategy to sort elements in an array.
 
-##### Explanation
+Info:
+* Use fot huge amount of data
+* Result provided avarage in 0(n log n) time
+* Result provided in worst case in 0(n2) time (if collection is already sorted)
+* It uses more space then Bubble Sort but less then Merge Sort
 
-**Quick Sort** is a highly efficient sorting algorithm that uses a divide-and-conquer strategy to sort elements in an array. You have to use:
-* pivot
-* partition()
+##### Hints
+Hints:
+* Use methods quickSort(array, start, end) and partition(array, start, end)
+* Use variables pivot, i, j
+* Use swapping with temp variable
+
+##### Complexity
+
+![My Image](readme-images/image-01.png)
--- interview/sorting-algorithms/java-quick-sort-asc-array-elements/src/Main.java
+++ interview/sorting-algorithms/java-quick-sort-asc-array-elements/src/Main.java
@@ -4,46 +4,50 @@ public class Main {
     
     public static void main(String[] args) {
         
-        int[] array = {3, 1, 6, 4, 7};                  // Input
+        int[] array = {3, 1, 6, 4, 7, 9};               // Input
 
         quickSort(array, 0, array.length - 1);    // Process
 
-        for (int i = 0; i < array.length; i++) {        // Output: 1, 3, 4, 6, 7
-            System.out.print(array[i] + " ");
+        for (int i : array) {                           // Output: 1, 3, 4, 6, 7, 9
+            System.out.print(i + " ");
         }
 
     }
 
     private static void quickSort(int[] array, int start, int end) {
-        
-        if (end <= start) return;
+
+        if (array == null || array.length == 0)
+            return;
+
+        if (end <= start) 
+            return;
 
         int pivot = partition(array, start, end);
         quickSort(array, start, pivot - 1);
         quickSort(array, pivot + 1, end);
-
+        
     }
 
     private static int partition(int[] array, int start, int end) {
-        
-        int pivot = array[end];
-        int i = start - 1;
-        int tmp;
 
-        for (int j = start; j <= end - 1; j++) {
+        int i = start - 1;
+        int pivot = array[end];
+
+        for (int j = 0; j < end - 1; j++) {
             
             if (array[j] < pivot) {
                 i++;
-                tmp = array[i];
+                int temp = array[i];
                 array[i] = array[j];
-                array[j] = tmp;
+                array[j] = temp;
             }
 
         }
+
         i++;
-        tmp = array[i];
+        int temp = array[i];
         array[i] = array[end];
-        array[end] = tmp;
+        array[end] = temp;
 
         return i;
 
