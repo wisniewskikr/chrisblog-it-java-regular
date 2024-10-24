e9c20e6e85b2f04093f5eb62edc600aedb1e8908
Chris <wisniewskikr@gmail.com>
2024-10-24 20:15:46 +0200

    Implement sum two

---
+++ interview/sum-two/Readme.md
@@ -0,0 +1,6 @@
+Please create following application:
+1. Input
+- Array of integers
+- Target as integer
+2. Output
+- Array of indexes two integers which sum is equal target
---
+++ interview/sum-two/src/main/java/com/example/App.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        
+        int[] array = {2, 4, 7, 6, 8};
+        int target = 12;                                                                                                // Input
+
+        int[] result = calculateSumTwo(array, target);                                                                  // Calculation
+
+        System.out.println("Index of first number is " + result[0] + " and index of second number is " + result[1]);    // Output: 1, 4
+
+    }
+
+    private static int[] calculateSumTwo(int[] array, int target) {
+        
+        if (array == null)
+            throw new IllegalArgumentException("Array can not be null");
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++) {
+            
+            int complement = target - array[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+
+            map.put(array[i], i);
+
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+
+    }
+}
