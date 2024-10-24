b35aa2b401b362cf7b2fcf2297879fc4e5fe1a61
Chris <wisniewskikr@gmail.com>
2024-10-24 19:42:35 +0200

    Implement Unit Tests

--- interview/reverse-polish-notation/src/test/java/com/example/AppTest.java
+++ interview/reverse-polish-notation/src/test/java/com/example/AppTest.java
@@ -1,20 +1,64 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldReturn6() {
+
+        String[] input = {"2", "4", "+"};       // Given
+
+        int result = App.calculateRPN(input);   // When
+
+        assertEquals(6, result);                // Then
+
     }
+
+    @Test
+    public void shouldReturn16() {
+
+        String[] input = {"2", "4", "*", "8", "+"};         // Given
+
+        int result = App.calculateRPN(input);               // When
+
+        assertEquals(16, result);                            // Then
+
+    }
+
+    @Test
+    public void shouldReturn24() {
+
+        String[] input = {"2", "4", "8", "+", "*"};         // Given
+
+        int result = App.calculateRPN(input);               // When
+
+        assertEquals(24, result);                            // Then
+
+    }
+
+    @Test
+    public void shouldReturn12() {
+
+        String[] input = {"2", "4", "8", "+", "*", "2", "/"};   // Given
+
+        int result = App.calculateRPN(input);                   // When
+
+        assertEquals(12, result);                               // Then
+
+    }
+
+    @Test
+    public void shouldReturnMinus8() {
+
+        String[] input = {"2", "4", "8", "-", "*"};             // Given
+
+        int result = App.calculateRPN(input);                   // When
+
+        assertEquals(-8, result);                               // Then
+
+    }
+
 }
