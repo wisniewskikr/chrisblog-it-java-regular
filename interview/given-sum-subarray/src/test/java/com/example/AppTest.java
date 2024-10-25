105cca36fa3f6178ff467b49c757a898a5ba0444
Chris <wisniewskikr@gmail.com>
2024-10-25 08:56:09 +0200

    Init JUnit test

--- interview/given-sum-subarray/src/test/java/com/example/AppTest.java
+++ interview/given-sum-subarray/src/test/java/com/example/AppTest.java
@@ -1,20 +1,23 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWith123() {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7};                // Given
+        int target = 6;
+
+        int[] result = App.givenSumSubarray(array, target);  // When
+
+        assertEquals(1, result[0]);                 // Then
+        assertEquals(2, result[1]);
+        assertEquals(3, result[2]);
+
     }
+
 }
