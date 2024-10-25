72537a0419d5d3d55e3c5d5521991216ee8a76f1
Chris <wisniewskikr@gmail.com>
2024-10-25 09:52:50 +0200

    Implement exception tests

--- interview/given-sum-subarray/src/test/java/com/example/AppTest.java
+++ interview/given-sum-subarray/src/test/java/com/example/AppTest.java
@@ -2,9 +2,14 @@ package com.example;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class AppTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
     
     @Test
     public void shouldAnswerWith123() {
@@ -17,6 +22,28 @@ public class AppTest {
         assertEquals(1, result[0]);                 // Then
         assertEquals(2, result[1]);
         assertEquals(3, result[2]);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldAnswerWithExceptionForNullArray() {
+
+        int[] array = null;                                  // Given
+        int target = 6;
+
+        App.givenSumSubarray(array, target);                 // When
+
+    }
+
+    @Test
+    public void shouldAnswerWithExceptionForNoSum() {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7};                                    // Given
+        int target = 100;
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("There is no given sum in the array");
+
+        App.givenSumSubarray(array, target);                                    // When
 
     }
 
