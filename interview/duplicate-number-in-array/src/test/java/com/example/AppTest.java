57b6bf1de32ad9022badd67d85bb731f9a503bf4
Chris <wisniewskikr@gmail.com>
2024-10-29 16:29:09 +0100

    Implement JUnit test

--- interview/duplicate-number-in-array/src/test/java/com/example/AppTest.java
+++ interview/duplicate-number-in-array/src/test/java/com/example/AppTest.java
@@ -1,20 +1,47 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    @Rule
+    public ExpectedException ee = ExpectedException.none();
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWith5(){
+        
+        int[] input = {2, 5, 3, 4, 8, 5, 9};                        // Given
+
+        int result = App.findDuplicateNumber(input);                // When
+
+        assertEquals(5, result);                            // Then
+
     }
+
+    @Test
+    public void shouldAnswerWithMinus1(){
+        
+        int[] input = {2, 5, 3, 4, 8, 6, 9};                        // Given
+
+        int result = App.findDuplicateNumber(input);                // When
+
+        assertEquals(-1, result);                                   // Then
+
+    }
+
+    @Test
+    public void shouldAnswerWithException(){
+        
+        int[] input = null;                                        // Given
+        ee.expect(IllegalArgumentException.class);
+        ee.expectMessage("Input can not be null");
+
+        App.findDuplicateNumber(input);                             // When
+
+    }
+
 }
