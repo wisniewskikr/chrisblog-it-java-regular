c4da81f16f1c1ed7995b41d551e1af66553f57b2
Chris <wisniewskikr@gmail.com>
2024-11-05 16:54:22 +0100

    Implement right rotate tests

--- interview/array-right-rotate/src/main/java/com/example/App.java
+++ interview/array-right-rotate/src/main/java/com/example/App.java
@@ -4,18 +4,18 @@ public class App {
 
     public static void main( String[] args ) {
 
-        int[] input = {1, 2, 3};                // Input
-        int rotate = 4;
+        int[] input = {1, 2, 3};                        // Input
+        int rotate = 2;
 
-        int[] result = rightRotate(input, rotate);
+        int[] result = rightRotate(input, rotate);      // Calculation
         
-        for (int i : result) {                   // Output: 3, 1, 2
+        for (int i : result) {                          // Output: 2, 3, 1
             System.out.print(i + " ");
         }
         
     }
 
-    private static int[] rightRotate(int[] input, int rotate) {
+    public static int[] rightRotate(int[] input, int rotate) {
 
         if (rotate == input.length)
             return input;
@@ -24,11 +24,11 @@ public class App {
             rotate = rotate % input.length;
         
         int[] result = new int[input.length];
-        int size = input.length - 1; 
-        int mid = size - rotate;     
+        int size = input.length - 1; // 6
+        int mid = size - rotate;     // 3
         int index = 0;
 
-        for (int i = size; i > mid; i--) {
+        for (int i = mid + 1; i <= size; i++) {
             result[index] = input[i];
             index++; 
         }
--- interview/array-right-rotate/src/test/java/com/example/AppTest.java
+++ interview/array-right-rotate/src/test/java/com/example/AppTest.java
@@ -1,20 +1,33 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue1() {
+        
+        int[] input = {1, 2, 3, 4, 5, 6, 7};
+        int rotate = 3;
+
+        int[] result = App.rightRotate(input, rotate);
+
+        assertArrayEquals(new int[]{5, 6, 7, 1, 2, 3, 4}, result);
+
     }
+
+    @Test
+    public void shouldAnswerWithTrue2() {
+        
+        int[] input = {1, 2, 3, 4, 5};
+        int rotate = 2;
+
+        int[] result = App.rightRotate(input, rotate);
+
+        assertArrayEquals(new int[]{4, 5, 1, 2, 3}, result);
+
+    }
+
 }
