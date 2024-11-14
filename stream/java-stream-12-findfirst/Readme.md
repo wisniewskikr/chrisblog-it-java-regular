bf9678aba68bcdb05c24c9f34ea9456ebd335452
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-11-14 08:06:51 +0100

    Init project

---
+++ stream/java-stream-12-findfirst/Readme.md
@@ -0,0 +1,15 @@
+##### Goal
+
+In Java, you can use the **Stream.min()** method to find the minimum element in a stream based on a specified comparator.
+
+##### Functional Interface
+
+N/A
+
+##### Result
+
+N/A
+
+##### Description
+
+Stream.min() is like "SELECT MIN(column)" for databases.
---
+++ stream/java-stream-12-findfirst/src/test/java/com/example/MainTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import java.util.List;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMinExample() {
+
+        List<Message> messages = Main.getMessages();                      // Given
+
+        Message result = Main.minExample(messages);                       // When
+
+        assertEquals(Long.valueOf(1), result.getId());                  // Then
+
+    }
+
+}
