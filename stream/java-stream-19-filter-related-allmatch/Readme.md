246dc05aad0d91579d8207ba40e17b81e94f2697
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-11-16 16:17:21 +0100

    Init project

---
+++ stream/java-stream-19-filter-related-allmatch/Readme.md
@@ -0,0 +1,15 @@
+##### Goal
+
+In Java, the **anyMatch()** method is a terminal operation of the Streams API. It checks whether any element in the stream matches a given predicate. If at least one element satisfies the condition, it returns true; otherwise, it returns false
+
+##### Functional Interface
+
+Predicate, boolean test(T)
+
+##### Result
+
+true/false
+
+##### Description
+
+Stream.anyMatch() is like "WHERE ... ANY ..." for databases.
---
+++ stream/java-stream-19-filter-related-allmatch/src/test/java/com/example/MainTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import java.util.List;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testAnyMatchExample() {
+
+        List<Message> messages = Main.getMessages();                                // Given
+
+        boolean result = Main.anyMatchExample(messages);                           // When
+
+        assertEquals(true, result);                                       // Then
+
+    }
+
+}
